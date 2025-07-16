6b93a787a4c108c6f2c674e65fece365f73e8f28
joshbaz <jkimbareeba@yahoo.com>
2025-07-16 22:55:49 +0300

    Updated the production url

--- src/pages/4.DirectMessages/DirectMessages.jsx
+++ src/pages/4.DirectMessages/DirectMessages.jsx
@@ -7,6 +7,7 @@ import { useGetSupervisorProfile } from '../../store/tanstackStore/services/quer
 import { Icon } from '@iconify/react';
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1';
+//const API_URL = import.meta.env.VITE_API_URL || 'http://drims.alero.digital/api/v1';
 
 // ========================================
 // UTILITY FUNCTIONS
